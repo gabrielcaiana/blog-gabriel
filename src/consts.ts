f89369c2e33550b53687713669646822542e67e4
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-02-10 11:42:27 -0300

    config project

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Gabriel Caiana - Blog";
+export const SITE_DESCRIPTION =
+  "Bem vindo ao meu blog pessoal. Aqui você encontrará artigos sobre tecnologia, programação, desenvolvimento web e muito mais.";
