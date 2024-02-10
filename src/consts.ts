f4eda81b986a52bd56e81ff4a4c2570d6be9ecb0
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-02-10 12:17:45 -0300

    feat: update content

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Gabriel Caiana - Blog";
+export const SITE_TITLE = "Gabriel Caiana";
 export const SITE_DESCRIPTION =
-  "Bem vindo ao meu blog pessoal. Aqui você encontrará artigos sobre tecnologia, programação, desenvolvimento web e muito mais.";
+  "Bem vindo ao meu site pessoal. Aqui você encontrará artigos sobre tecnologia, programação, desenvolvimento web e muito mais.";
