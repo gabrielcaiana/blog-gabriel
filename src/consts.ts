c0c1fb75fe92f5d7e7ba06719f1650fa6cd1b3ab
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-02-23 21:31:47 -0300

    chore: update site title and add view transitions

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Gabriel Caiana";
+export const SITE_TITLE = "Gabriel Caiana | Frontend Engineer";
 export const SITE_DESCRIPTION =
   "Bem vindo ao meu site pessoal. Aqui você encontrará artigos sobre tecnologia, programação, desenvolvimento web e muito mais.";
