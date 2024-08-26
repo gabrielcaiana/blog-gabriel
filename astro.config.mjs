9faae303159de240845c203e7168d3f719e789ff
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-08-26 18:48:12 -0300

    fix: defineConfig site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.gabrielcaiana.com",
+  site: "https://gabrielcaiana.com",
   
   integrations: [mdx(), sitemap()],
 });
